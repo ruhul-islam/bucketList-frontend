c814e0535799f649b4090b2fe66a9eeba8e5410d
Lex-G <alexagrim95@gmail.com>
2022-08-29 06:12:11 -0700

    nav bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,13 @@
+import "./Nav.css";
+
+const Nav = () => {
+  return (
+    <ul className="Nav">
+      <li>Home</li>
+      <li>Friends</li>
+      <li>Events</li>
+    </ul>
+  );
+};
+
+export default Nav;
