d9e6b5abfb9a3cb73c79fff41ed0d7f834954ef4
Chris Calleja <callejac1@gmail.com>
2022-08-31 13:21:32 -0400

    chris user

--- src/services/bucketListService.ts
+++ src/services/bucketListService.ts
@@ -1,12 +1,16 @@
 import axios from "axios";
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 import BucketListIdea from "../models/BucketListIdea";
 import BucketListItem from "../models/BucketListItem";
 
-const baseURL: string = "https://api.api-ninjas.com/v1/bucketlist";
-const localURL: string = process.env.REACT_APP_API_URL || "";
+const { user } = useContext(AuthContext);
+
+const ninjaURL: string = "https://api.api-ninjas.com/v1/bucketlist";
+const baseURL: string = process.env.REACT_APP_API_URL || "";
 export const fetchBucketListIdea = async (): Promise<BucketListIdea> => {
   return axios
-    .get(baseURL, {
+    .get(ninjaURL, {
       headers: { "X-Api-Key": "HM9OmkV1/SV/PJWXneA8eg==58pPfU9PN8ty65RU" },
     })
     .then((response) => {
@@ -15,16 +19,19 @@ export const fetchBucketListIdea = async (): Promise<BucketListIdea> => {
 };
 
 export const fetchBucketList = async (): Promise<BucketListItem[]> => {
-  return (await axios.get(localURL)).data;
+  return (await axios.get(baseURL)).data;
 };
 
 export const addBucketListItem = async (
   item: BucketListItem
 ): Promise<BucketListItem> => {
-  return (await axios.post(localURL, item)).data;
+  return (await axios.post(baseURL, item)).data;
 };
 
 export const deleteById = async (id: string): Promise<void> => {
-  return (await axios.delete(`${localURL}/user/${encodeURIComponent(id)}`))
-    .data;
+  return (await axios.delete(`${baseURL}/user/${encodeURIComponent(id)}`)).data;
+};
+
+export const addUser = async (): Promise<void> => {
+  return (await axios.post(`${baseURL}/user`, user)).data;
 };
